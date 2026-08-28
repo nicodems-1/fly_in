a9363a5cf89a28b42995807c4b7cc88460ce1396
niverdie <niverdie@DESKTOP-A88FCJC.localdomain>
2026-08-28 16:16:28 +0200

    parsing working for list of connections and dictionnary of hubs, need to implement metadata processing now

--- build/srcs/models.py
+++ build/srcs/models.py
@@ -6,7 +6,6 @@ class Hub(BaseModel):
     '''class hub which contains info for each hub such as color position, name and role'''
     x: int
     y: int
-    color: Optional[str] = None
     name: str
     role: Literal["start_hub", "end_hub", "hub"]
 
@@ -18,16 +17,18 @@ class Connection(BaseModel):
 class Context(BaseModel):
     '''context contain all the infos from class hub and class connection, we'll use context
     for the calculations and the display'''
-    nb_of_drones: PositiveInt
+    nb_drones: PositiveInt
     hubs: dict[str, Hub]
     connections: list[Connection]
 
 class HubMetadata(BaseModel):
+    model_config = {"extra": "forbid"}
     color: Optional[str] = None
     max_drones: Optional[PositiveInt] = None
     zone: Optional[Literal["normal", "restricted", "priority", "blocked"]] = (None)
 
 
 class ConnectionMetadata(BaseModel):
+    model_config = {"extra": "forbid"}
     max_link_capacity: Optional[PositiveInt] = None
     
--- build/srcs/parser.py
+++ build/srcs/parser.py
@@ -1,28 +1,59 @@
-from models import Hub, Connection, Context, Metadata
+from models import Hub, Connection, Context, HubMetadata, ConnectionMetadata
 
-def parsing():
-    nb_of_drones = 0
+def parsing_meta(metadata: str)->dict[str, str] or None:
+    meta_dict = {}
+    if(metadata.endswith("]")):
+        clean_meta = metadata.strip("]")
+        splitted_meta = clean_meta.split()
+        for item in splitted_meta:
+            key, val = item.split("=")
+            meta_dict[key] = val
+        print(f"{meta_dict} metadict_connection")
+        return(meta_dict)
+    else:
+        raise ValueError("Error line 33, metadata format does not correspond")
+
+def parsing() -> Context:
+    nb_drones = 0
     hubs: dict[str, Hub] = {}
-    connnection: Connection = []
+    connections: list[Connection] = []
     with open("maps/easy/01_linear_path.txt") as f:
         for x in f:
             if x.startswith("#"):
-                pass
+                continue
             if x.startswith("nb_drones"):
-                nb_of_drones = int(x.split(":")[1].strip())
+                nb_drones = int(x.split(":")[1].strip())
             if(x.startswith("start_hub") or x.startswith("hub") or x.startswith("end_hub")):
-                splitted = x.strip().split()
-                print("splitted", splitted)
-                role = splitted[0].strip(":")
-                name = splitted[1]
-                x_coor = splitted[2]
-                y_coor = splitted[3]
-                if(len(splitted) == 5):
-                    parsing_metadata(splitted[4].strip("\n"))
-                print (f"{role}\n{name}\n{x_coor}\n{y_coor}\n{metadata}\n")
+                hub_split = x.strip().split()
+                if("[" in x):
+                    split_meta = x.split("[")
+                    hub_data = split_meta[0]
+                    metadata = split_meta[1].strip()
+                    parsing_meta(metadata)
+                    hub_split = hub_data.strip().split()
+                x_coor = hub_split[2]
+                y_coor = hub_split[3]
+                name = hub_split[1]
+                role = hub_split[0].strip(":")
+                my_hub = Hub(x=x_coor, y=y_coor, name=name, role=role)
+                hubs.update({name: my_hub})
+            if(x.startswith("connection")):
+                co_data = x.split(":")[1]
+                if("[" in x):
+                    split_meta = x.split("[")
+                    co_data = split_meta[0]
+                    metadata = split_meta[1].strip()
+                    parsing_meta(metadata)
+                co_split = co_data.strip().split("-")
+                source = co_split[0]
+                target = co_split[1]
+                my_connection = Connection(source=source, target=target)
+                connections.append(my_connection)
+        print(connections, hubs)
+        return(Context(nb_drones=nb_drones, hubs=hubs, connections=connections))
 
 parsing()
 
-def parsing_metadata(metadata: str):
-    
+
+        
 
