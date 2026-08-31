0b4ebe27ea74c4304690681ae262ebe031cbd290
Nicodeme Verdier <niverdie@z1r2p5.42lyon.fr>
2026-08-31 21:31:02 +0200

    map loading circles, still need to load lines and calcul the scale

--- build/srcs/load_map.py
+++ build/srcs/load_map.py
@@ -4,9 +4,9 @@ from models import Context, Hub
 class MapVisualizer():
     def __init__(self):
         self.root = tk.Tk()
-        screen_width= self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        self.canvas = tk.Canvas(self.root, width=screen_width*0.7, height=screen_height*0.7, borderwidth=0, highlightthickness=0,
+        self.screen_width= self.root.winfo_screenwidth()*0.7
+        self.screen_height = self.root.winfo_screenheight()*0.7
+        self.canvas = tk.Canvas(self.root, width=self.screen_width, height=self.screen_height, borderwidth=0, highlightthickness=0,
                        bg="white")
 
     def _create_circle(self, x, y, r, **kwargs):
@@ -14,8 +14,21 @@ class MapVisualizer():
 
     def create_hubs(self, context: Context):
         hubs = context.hubs
+        x_max = max([hub.x for hub in hubs.values()])
+        x_min = min([hub.x for hub in hubs.values()])
+        y_max = max([hub.y for hub in hubs.values()])
+        y_min = min([hub.y for hub in hubs.values()])
+        print(f"screen height    {self.screen_height}")
+        print(f"screen width    {self.screen_width}")
+        scale = 100
+        circle_size = 50
+        offset_x = (self.screen_width - (x_max - x_min)*scale)/2
         for hub in hubs.values():
-            self._create_circle(hub.x*100 + 100, hub.y*100 + 300, 30)
+            x = hub.x*scale + offset_x
+            y = self.screen_height/2 - hub.y*scale
+            self._create_circle(x, y, circle_size, fill=hub.metadata.color)
+            self.canvas.create_text(x,y,fill="white",font="Times 10 italic bold",
+                            text=hub.name)
 
     def load_map(self, context):
         self.canvas.grid()
--- build/srcs/main.py
+++ build/srcs/main.py
@@ -2,6 +2,6 @@ from parser import parsing
 from models import Context
 from load_map import MapVisualizer
 
-my_context = parsing("maps/hard/02_capacity_hell.txt")
+my_context = parsing("maps/hard/03_ultimate_challenge.txt")
 my_visual = MapVisualizer()
 my_visual.load_map(my_context)
