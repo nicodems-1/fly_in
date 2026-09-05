6a9f3f631f39a6f3416e295122a46d0cfe2f47b7
niverdie <verdiernicodeme@gmail.com>
2026-09-05 13:32:45 +0200

    making a plan for later and optimizing the visualizer

--- src/load_map.py
+++ src/load_map.py
@@ -9,6 +9,11 @@ class MapVisualizer():
         self.img = tk.PhotoImage(file="drone.png").subsample(35)
         self.canvas = tk.Canvas(self.root, width=self.screen_width, height=self.screen_height, borderwidth=0, highlightthickness=0,
                        bg="white")
+        self.x_min = 0
+        self.x_max = 0
+        self.y_min = 0
+        self.y_max = 0
+        self.scale = 0
 
     def spawn_png(self):
         image = self.canvas.create_image(100, 100, anchor=tk.NW, image=self.img)
@@ -16,37 +21,53 @@ class MapVisualizer():
     def _create_circle(self, x, y, r, **kwargs):
         return self.canvas.create_oval(x-r, y-r, x+r, y+r, **kwargs)
 
+    def setup_map_data(self, context: Context):
+        hubs = context.hubs
+        connections = context.connections
+
+        self.x_max = max([hub.x for hub in hubs.values()])
+        self.x_min = min([hub.x for hub in hubs.values()])
+
+        self.y_max = max([hub.y for hub in hubs.values()])
+        self.y_min = min([hub.y for hub in hubs.values()])
+
+        dx = (self.x_max - self.x_min) or 1
+        dy = (self.y_max - self.y_min) or 1
+
+        scale_x = self.screen_width / dx
+        scale_y = self.screen_height / dy
+        full_scale = min(scale_x, scale_y)
+        padding = (min(scale_x, scale_y)/3)
+        final_scale = full_scale - padding
+        self.scale = final_scale
+
+    def get_x_real_coords(self, x_coords):
+        offset_x = (self.screen_width - ((self.x_max-self.x_min)*self.scale))/2
+        return((x_coords - self.x_min)*self.scale + offset_x)
+
+    def get_y_real_coords(self, y_coords):
+        offset_y = (self.screen_height - ((self.y_max - self.y_min)*self.scale) + 2 * (self.scale/3))/2
+        return((self.y_max - y_coords)*self.scale + offset_y)
+
     def create_hubs(self, context: Context):
         hubs = context.hubs
         connections = context.connections
-        x_max = max([hub.x for hub in hubs.values()])
-        x_min = min([hub.x for hub in hubs.values()])
-        y_max = max([hub.y for hub in hubs.values()])
-        y_min = min([hub.y for hub in hubs.values()])
-
-        dx = (x_max - x_min) or 1
-        dy = (y_max - y_min) or 1
-        scale_x = self.screen_width / dx
-        scale_y = self.screen_height / dy
-
-        scale = min(scale_x, scale_y) - (min(scale_x, scale_y)/3)
-        circle_size = scale/3
-        offset_x = (self.screen_width - (dx*scale))/2
-        offset_y = (self.screen_height - (dy*scale) + 2 * circle_size)/2
+        circle_size = self.scale/3
         for connection in connections:
-            x0 = ((hubs[connection.source].x) - x_min)*scale + offset_x
-            y0 = (y_max - (hubs[connection.source].y))*scale + offset_y
-            x1 = ((hubs[connection.target].x) - x_min)*scale + offset_x
-            y1 = (y_max - (hubs[connection.target].y))*scale + offset_y
+            x0 = self.get_x_real_coords(hubs[connection.source].x)
+            y0 = self.get_y_real_coords(hubs[connection.source].y)
+            x1 = self.get_x_real_coords(hubs[connection.target].x)
+            y1 = self.get_y_real_coords(hubs[connection.target].y)
             self.canvas.create_line(x0, y0, x1, y1)
         for hub in hubs.values():
-            x = (hub.x - x_min)*scale + offset_x
-            y = (y_max - hub.y)*scale + offset_y
+            x = self.get_x_real_coords(hub.x)
+            y = self.get_y_real_coords(hub.y)
             self._create_circle(x, y, circle_size, fill=hub.metadata.color)
             self.canvas.create_text(x,y,fill="white",font="Times 10 italic bold",
                             text=hub.name)
 
     def load_map(self, context):
+        self.setup_map_data(context)
         self.canvas.grid()
         self.spawn_png()
         self.create_hubs(context)
